67220e558c0243d09bd0328a71d5647ec7ea4c55
Lim Swee Kiat <limsweekiat@gmail.com>
2019-06-12 11:56:24 +0800

    minified images

--- assets/make_dict.py
+++ assets/make_dict.py
@@ -1,9 +1,26 @@
 import os
 import json
+from PIL import Image
 
-filenames = []
-for filename in os.listdir("imagenet_dogs"):
-	filenames.append(filename)
+def make_dict():
+	filenames = []
+	for filename in os.listdir("imagenet_dogs"):
+		filenames.append(filename)
 
-with open("imagenet_dogs_dict.js", "w") as file:
-	json.dump(filenames, file)
+	with open("imagenet_dogs_dict.js", "w") as file:
+		json.dump(filenames, file)
+
+def minify():
+	size = [100, 100]
+	folder = "imagenet_dogs/"
+	newfolder = "small_dogs/"
+	for filename in os.listdir(folder):
+		im = Image.open(folder + filename)
+		s = im.size
+		scale = max(s) / min(s)
+		newsize = [scale * dim for dim in size]
+		im.thumbnail(newsize, Image.BICUBIC)
+		im.save(newfolder + filename, "JPEG")
+
+if __name__ == "__main__":
+	minify()
